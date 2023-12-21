48fd42248126f97f993f084578a10b2848194d5b
walterdevreis <walter-reis@outlook.com>
2023-12-21 10:54:51 -0300

    Classe PaypalService testada e finalizada

--- src/Program.java
+++ src/Program.java
@@ -2,14 +2,21 @@ package src;
 
 import src.entities.Contract;
 import src.entities.Installment;
+import src.services.OnlinePaymentService;
+import src.services.PaypalService;
 
 import java.util.Date;
+import java.util.Locale;
 
 public class Program {
     public static void main(String args[]){
 
-        Installment installment = new Installment(new Date(), 600.00);
-        Contract contract = new Contract();
+        Locale.setDefault(Locale.US);
+
+        OnlinePaymentService service = new PaypalService();
+        double resultado = service.interest(200.0, 3);
+        System.out.printf("%.2f", service.paymentFee(resultado));
+
 
     }
 }
